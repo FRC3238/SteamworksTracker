863d8517f231680747d591d3294f7c087e41a741
Graeth <smithgar002@gmail.com>
2017-02-20 04:52:19 -0800

    Generates points in waypoint format!

---
+++ src/ValueHolder.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * Created by Julee on 2/20/2017.
+ */
+public class ValueHolder {
+    public ValueHolder() {}
+    public void drawConstants(Graphics g) {
+        g.drawRect(100,650, 50, 16);
+    }
+}
